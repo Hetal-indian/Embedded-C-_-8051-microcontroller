66e29772c55f28bf7b7f27d4cf8fee686304275d
Hetal_ <72317257+Hetal-indian@users.noreply.github.com>
2022-12-23 16:25:38 +0530

    Update led.c

--- led.c
+++ led.c
@@ -1,17 +1,15 @@
 #include<reg51.h>
-sbit a= P3^0;
-
 void main()
 {
-unsigned char n[10]={0X06,0XF9,0X24,0X30,0X19,0X12,0X02,0XF8,0XE00,0X10};
+unsigned char n[10]={0X3F,0x06,0X5B,0X4f,0x7f};
 unsigned int i,j;
-a=1;
+
 while(1)
 {
-for(i=0;i<10;i++)
+for(i=0;i<4;i++)
 {
-//P2=n[i];
-	P2=0X5C;
+P2=n[i];
+	
 for(j=0;j<60000;j++);
 }
 }
